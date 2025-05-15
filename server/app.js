8a330bf00db2afd01786ce5e11c6aecd9fc9e795
Bayu Ragil Pangestu <bayuragilpangestu8677@gmail.com>
2025-05-15 16:18:23 +0700

    cors

--- server/app.js
+++ server/app.js
@@ -1,58 +1,77 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
-import cookieParser from 'cookie-parser'
-import helmet from 'helmet'
-import ExpressMongoSanitize from 'express-mongo-sanitize'
-import { v2 as cloudinary } from 'cloudinary';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import { v2 as cloudinary } from "cloudinary";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+import cors from "cors";
+
+const allowedOrigins = [
+  "https://wanfood-dr59.vercel.app",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 //Middleware
-app.use(express.json())
-app.use(helmet())
-app.use(ExpressMongoSanitize())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('./public')) 
+app.use(express.json());
+app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
 
-import authRouter from './routes/authRouter.js'
-import productRouter from './routes/productRouter.js'
-import OrderRouter from './routes/orderRouter.js'
+import authRouter from "./routes/authRouter.js";
+import productRouter from "./routes/productRouter.js";
+import OrderRouter from "./routes/orderRouter.js";
 
 // Parent Router
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/product', productRouter)
-app.use('/api/v1/order', OrderRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/order", OrderRouter);
 
 // Root path handler
-app.get('/', (req, res) => {
-  res.json({ message: 'API berhasil dijalankan!' });
+app.get("/", (req, res) => {
+  res.json({ message: "API berhasil dijalankan!" });
 });
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 // Server
 app.listen(port, () => {
-  console.log(`aplikasi jalan di port ${port}`)
-})
+  console.log(`aplikasi jalan di port ${port}`);
+});
 
 // Connection DB
-mongoose.connect(process.env.DATABASE, {
-
-}).then(() => {
-    console.log("Database Connect")
-}).catch((err) => {
-
-})
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => {
+    console.log("Database Connect");
+  })
+  .catch((err) => {});
